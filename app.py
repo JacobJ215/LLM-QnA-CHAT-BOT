179d9cf35c8e7c737ec8be20556f6f541d223077
JacobJ215 <jacob.johnson2718@gmail.com>
2023-10-10 20:09:39 -0500

    updated UI

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def create_conversation_chain(vector_store):
     return conversation_chain
 
 # Create qna function
-def qna(query, chain):
+def conversational_chat(query, chain):
     result = chain({"question": query, "chat_history": st.session_state['history']})
     st.session_state['history'].append((query, result["answer"]))
     return result["answer"]
@@ -101,10 +101,10 @@ def main():
                 #st.session_state.conversation = create_conversation_chain(vector_store)
                 chain = create_conversation_chain(vector_store)
 
-                def conversational_chat(query):
-                    result = chain({"question": query, "chat_history": st.session_state['history']})
-                    st.session_state['history'].append((query, result["answer"]))
-                    return result["answer"]
+                # def conversational_chat(query):
+                #     result = chain({"question": query, "chat_history": st.session_state['history']})
+                #     st.session_state['history'].append((query, result["answer"]))
+                #     return result["answer"]
                 
                 if 'history' not in st.session_state:
                     st.session_state['history'] = []
@@ -123,11 +123,11 @@ def main():
                 with container:
                     with st.form(key='my_form', clear_on_submit=True):
                         
-                        user_input = st.text_input("Query:", placeholder="Talk to your csv data here (:", key='input')
+                        user_input = st.text_input("Query:", placeholder="Ask about me about your document (:", key='input')
                         submit_button = st.form_submit_button(label='Send')
                         
                     if submit_button and user_input:
-                        output = conversational_chat(user_input)
+                        output = conversational_chat(user_input, chain)
                         
                         st.session_state['past'].append(user_input)
                         st.session_state['generated'].append(output)
@@ -135,8 +135,8 @@ def main():
                 if st.session_state['generated']:
                     with response_container:
                         for i in range(len(st.session_state['generated'])):
-                            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-                            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+                            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                            message(st.session_state["generated"][i], key=str(i))
 
 
 
